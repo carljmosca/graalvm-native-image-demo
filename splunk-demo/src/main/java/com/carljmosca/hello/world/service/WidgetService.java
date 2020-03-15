6f9ee28ca3c756c23e5f2f639b0433f029a9647f
Carl J. Mosca <carl_mosca@ao.uscourts.gov>
2020-03-15 09:33:19 -0400

    created branch

---
+++ splunk-demo/src/main/java/com/carljmosca/hello/world/service/WidgetService.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.carljmosca.hello.world.service;
+
+import com.carljmosca.hello.world.model.Widget;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class WidgetService {
+    
+    private static final Logger LOGGER = LogManager.getLogger("WidgetService.class");
+    private static final int WIDGETS = 10;
+    private final List<Widget> widgets;
+    
+    public WidgetService() {
+        widgets = new ArrayList<>();
+    }
+    
+    public void create() {
+        
+        for (int i = 0; i < WIDGETS; i++) {
+            LOGGER.info(String.format("Creating widget %d", i));
+            widgets.add(new Widget(i, String.format("Name %d", i)));
+        }
+    }
+    
+    public void display() {
+        LOGGER.info("Displaying widgets");
+        widgets.forEach((widget) -> {
+            System.out.println(widget.toString());
+        });
+    }
+}
